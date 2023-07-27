493e608dfbda2d31dbda14b5d31e2a62a905b78d
chenxiweiguang <48124858+chenxiweiguang@users.noreply.github.com>
2023-07-27 15:11:04 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex bd-highlight" style="background-color: #17c4ff;">
+  <div class="p-2 flex-fill bd-highlight">
+    <div class="p-2 bd-highlight"><Chart1></Chart1></div>
+  <div class="p-2 bd-highlight"><Chart2></Chart2></div>
+  </div>
+  <div class="p-2 flex-fill bd-highlight"><China></China></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <div class="p-2 bd-highlight"><Chart3></Chart3></div>
+  <div class="p-2 bd-highlight"><Chart4></Chart4></div>
+    </div>
+</div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Chart1 from './components/Chart1.vue'
+import Chart2 from './components/Chart2.vue'
+import Chart3 from './components/Chart3.vue'
+import Chart4 from './components/Chart4.vue'
+import Maps from './components/Maps.vue'
+import China from './components/China.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Chart1,
+    Chart2,
+    Chart3,
+    Chart4,
+    Maps,
+    China
+  }
+}
+</script>
---
+++ src/components/China.vue
@@ -0,0 +1,171 @@
+<template>
+    <h1>地图</h1>
+    <div ref="mychart" style="width: 400px;height:400px;"></div>
+
+
+</template>
+  
+<script>
+import * as echarts from 'echarts'
+import chinaMap from '../assets/chinaMap.json'
+// let chinaMap=import('../src/assets/chainMap.json')
+// console.log(chinaMap)
+echarts.registerMap('china', chinaMap);
+let points = [
+  { value: [87.628579, 43.793301], itemStyle: { color: '#00EEFF' } },
+  { value: [104.076452, 30.651696], itemStyle: { color: '#00EEFF' } },
+  { value: [103.826777, 36.060634], itemStyle: { color: '#00EEFF' } },
+  { value: [102.709372, 25.046432], itemStyle: { color: '#00EEFF' } },
+  { value: [108.327537, 22.816659], itemStyle: { color: '#00EEFF' } },
+  { value: [112.982951, 28.116007], itemStyle: { color: '#00EEFF' } },
+  { value: [117.020725, 36.670201], itemStyle: { color: '#00EEFF' } },
+  { value: [113.753094, 34.767052], itemStyle: { color: '#00EEFF' } },
+  { value: [112.578781, 37.813948], itemStyle: { color: '#00EEFF' } },
+  { value: [119.296194, 26.101082], itemStyle: { color: '#00EEFF' } },
+  { value: [120.152575, 30.266619], itemStyle: { color: '#00EEFF' } },
+  { value: [118.763563, 32.061377], itemStyle: { color: '#00EEFF' } },
+  { value: [116.407387, 39.904179], itemStyle: { color: '#00EEFF' } },
+  { value: [108.953939, 34.266611], itemStyle: { color: '#00EEFF' } },
+  { value: [113.266887, 23.133306], itemStyle: { color: '#FFFA00' } },
+]
+  export default {
+    // name: 'Chart1',
+    mounted(){
+        let myEcharts=echarts.init(this.$refs.mychart)
+        myEcharts.setOption({
+  // backgroundColor: '#FFFFFF',
+  geo: {
+    map: 'china',
+    aspectScale: 0.75, // 长宽比
+    zoom: 1.1,
+    roam: false,
+    regions: [{
+      name: '南海诸岛',
+      itemStyle: {
+        areaColor: 'rgba(0, 10, 52, 1)',
+        borderColor: 'rgba(0, 10, 52, 1)',
+        normal: {
+          opacity: 0,
+          label: {
+            show: false,
+            color: '#009cc9',
+          },
+        },
+      },
+
+    }],
+  },
+  series: [{
+      type: 'map',
+      roam: false,
+      label: {
+        normal: {
+          show: true,
+          textStyle: {
+            color: '#fff',
+          },
+        },
+        emphasis: {
+          textStyle: {
+            color: '#fff',
+          },
+        },
+      },
+      itemStyle: {
+        normal: {
+          borderColor: '#0068FB',
+          borderWidth: 1,
+          areaColor: {
+            type: 'radial',
+            x: 0.5,
+            y: 0.5,
+            r: 0.8,
+            colorStops: [{
+              offset: 0,
+              color: '#0C3994', // 0% 处的颜色
+            }, {
+              offset: 1,
+              color: '#0C3994', // 100% 处的颜色
+            }],
+            globalCoord: true, // 缺省为 false
+          },
+        },
+        emphasis: {
+          areaColor: '#0068FB',
+          borderWidth: 0.1,
+        },
+      },
+      zoom: 1.1,
+      map: 'china', // 使用
+    },
+    {
+      type: 'effectScatter',
+      coordinateSystem: 'geo',
+      showEffectOn: 'render',
+      zlevel: 1,
+      rippleEffect: {
+        number: 3, // 波纹的数量。
+        period: 5, // 动画的周期，秒数。
+        scale: 17, // 动画中波纹的最大缩放比例。
+        brushType: 'fill', // 波纹的绘制方式，可选 'stroke' 和 'fill'。
+      },
+      symbolSize: 2,
+      data: points,
+    }, // 地图线的动画效果
+    {
+      type: 'effectScatter',
+      coordinateSystem: 'geo',
+      showEffectOn: 'render',
+      zlevel: 1,
+      rippleEffect: {
+        number: 4,
+        period: 5,
+        scale: 21,
+        brushType: 'fill',
+      },
+      symbolSize: 3,
+      data: [{ value: [113.266887, 23.133306], itemStyle: { color: '#FFFA00' } }],
+    },
+    {
+      type: 'lines',
+      zlevel: 2,
+      effect: {
+        show: true,
+        period: 4, // 箭头指向速度，值越小速度越快
+        trailLength: 0.4, // 特效尾迹长度[0,1]值越大，尾迹越长重
+        symbol: 'pin', // 箭头图标
+        symbolSize: 6, // 图标大小
+      },
+      lineStyle: {
+        normal: {
+          color: '#1DE9B6',
+          width: 1, // 线条宽度
+          opacity: 0.1, // 尾迹线条透明度
+          curveness: 0.3, // 尾迹线条曲直度
+        },
+      },
+      data: [
+        { coords: [[113.266887, 23.133306], [87.628579, 43.793301]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [104.076452, 30.651696]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [103.826777, 36.060634]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [102.709372, 25.046432]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [108.327537, 22.816659]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [112.982951, 28.116007]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [117.020725, 36.670201]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [113.753094, 34.767052]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [112.578781, 37.813948]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [119.296194, 26.101082]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [120.152575, 30.266619]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [118.763563, 32.061377]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [116.407387, 39.904179]], lineStyle: { color: '#FFF96B' } },
+        { coords: [[113.266887, 23.133306], [108.953939, 34.266611]], lineStyle: { color: '#FFF96B' } },
+      ],
+    },
+    ],
+})
+    }
+    
+  }
+</script>
+<style>
+</style>
